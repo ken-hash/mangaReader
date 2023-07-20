e400f83a877e0cab7fad13f9e0446d4b280c2cd8
Kenn <ken@ken.com>
2023-07-21 00:52:25 +0800

    Using views for performance increase

---
+++ MangaReaderBareBone/DTO/MangaLogsChapterView.cs
@@ -0,0 +1,12 @@
+﻿namespace MangaReaderBareBone.Models
+{
+    public class MangaLogsChapterView
+    {
+        public int MangaId { get; set; }
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+        public DateTime LogDateTime { get; set; }
+        public int MangaChaptersId { get; set; }
+        public string? ChapterName { get; set; }
+    }
+}
